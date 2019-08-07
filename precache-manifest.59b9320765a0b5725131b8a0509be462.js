454097967073037005b5ab704edbf69dc7aa1ee4
hahaluckyme <brimfoxxo@gmail.com>
2019-08-06 20:52:32 -0700

    Updates

---
+++ precache-manifest.59b9320765a0b5725131b8a0509be462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1084a68d2b157e2e50d8",
+    "url": "/static/css/main.6aeffe05.chunk.css"
+  },
+  {
+    "revision": "1084a68d2b157e2e50d8",
+    "url": "/static/js/main.1084a68d.chunk.js"
+  },
+  {
+    "revision": "dc87eda1bb7ece0081bd",
+    "url": "/static/js/1.dc87eda1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29bb540cb99f0e93b2e03ffab2145ae0",
+    "url": "/index.html"
+  }
+];
